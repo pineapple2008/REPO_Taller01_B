9b8c1e0a9794beb39afe20baf27a151dd00b3e22
pineapple2008 <sebastiaanalbertopinacuetocrg@gmail.com>
2026-04-17 14:18:48 -0400

    creando el proyecto persistencia

---
+++ Persistencia01/Persistencia01/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: usuario
+ * Fecha: 17/04/2026
+ * Hora: 14:16
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace Persistencia01
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
